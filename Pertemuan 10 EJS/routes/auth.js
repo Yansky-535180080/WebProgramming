74f22dad1fdccb32de53d5e5d9c7f653f9195ecc
syansky <syansky@yahoo.com>
2020-11-18 20:32:06 +0700

    Pertemuan 10

---
+++ Pertemuan 10 EJS/routes/auth.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/login', async (req, res) => {
+    if (req.session.user) {
+        res.redirect('/');
+    }
+    else {
+        res.render('pages/login', { layout: false});
+    }
+});
+
+router.get('/logout', async (req, res) => {
+    // destroy all sessions
+    req.session.destroy();
+
+    //redirect to login
+    res.redirect('/auth/login');
+});
+
+router.post('/login', async (req, res) => {
+    // get user input
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username === "admin" && password === "admin") {
+        // TODO: implement sessions to check user is logged-in
+        req.session.user = "admin";
+
+        // redirect to member area
+        res.redirect('/');
+    }
+    else {
+        // TODO: render the login page with error information
+        res.render('pages/login', {layout: false, error: 'Wrong username or password.'});
+    }
+})
+
+module.exports = router;
---
+++ Pertemuan 10 EJS/routes/todo.js
@@ -0,0 +1,37 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    if (!req.session.user) {
+        res.redirect('auth/login');
+    }
+    else {
+        res.render('pages/todo', { tasks: req.session.tasks });
+    }
+});
+
+router.post('/add', async (req, res) => {
+    // if there's no task in the session, create one
+    if (!req.session.tasks) {
+        req.session.tasks = [];
+    }
+    const newTask = req.body.taskName;
+    req.session.tasks.push(newTask);
+
+    res.redirect('/todo');
+});
+
+router.post('/done/:index', async (req, res) => {
+    // get the index of to be deleted task
+    const index = req.params.index;
+
+    // only delete if there's that task
+    if (req.session.tasks && index < req.session.tasks.length) {
+        req.session.tasks.splice(index, 1);
+    }
+
+    res.redirect('/todo');
+});
+
+module.exports = router;
